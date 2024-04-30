565d086a00dc6dd4a5559b69ee87f2a022d56a43
Allyson Hamilton <157050625+allyson10500@users.noreply.github.com>
2024-04-30 11:14:18 -0500

    Add files via upload

---
+++ Introduction_to_Regression_with_Statsmodels_in_Python.py
@@ -0,0 +1,507 @@
+# Import seaborn with alias sns
+import seaborn as sns
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Import seaborn with alias sns
+import seaborn as sns
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Draw the scatter plot
+sns.scatterplot(x="n_convenience",
+                y="price_twd_msq",
+                data=taiwan_real_estate)
+
+# Draw a trend line on the scatter plot of price_twd_msq vs. n_convenience
+sns.regplot(x="n_convenience",
+                y="price_twd_msq",
+                data=taiwan_real_estate,
+         ci=None,
+         scatter_kws={'alpha': 0.5})
+
+# Show the plot
+plt.show()
+
+# Import the ols function
+from statsmodels.formula.api import ols
+
+# Create the model object
+mdl_price_vs_conv = ols("price_twd_msq~n_convenience", data=taiwan_real_estate)
+
+# Fit the model
+mdl_price_vs_conv = mdl_price_vs_conv.fit()
+
+# Print the parameters of the fitted model
+print(mdl_price_vs_conv.params)
+
+# Histograms of price_twd_msq with 10 bins, split by the age of each house
+sns.displot(data=taiwan_real_estate,
+         x="price_twd_msq",
+         col="house_age_years",
+         col_wrap=3,
+         bins = 10)
+
+# Show the plot
+plt.show()
+
+# Calculate the mean of price_twd_msq, grouped by house age
+mean_price_by_age = taiwan_real_estate.groupby("house_age_years")["price_twd_msq"].mean()
+
+# Print the result
+print(mean_price_by_age)
+
+# Create the model, fit it
+mdl_price_vs_age = ols("price_twd_msq~house_age_years", data=taiwan_real_estate).fit()
+
+# Print the parameters of the fitted model
+print(mdl_price_vs_age.params)
+
+# Update the model formula to remove the intercept
+mdl_price_vs_age0 = ols("price_twd_msq ~ house_age_years + 0", data=taiwan_real_estate).fit()
+
+# Print the parameters of the fitted model
+print(mdl_price_vs_age0.params)
+
+# Import numpy with alias np
+import numpy as np
+
+# Create the explanatory_data 
+explanatory_data = pd.DataFrame({'n_convenience': np.arange(0,11)})
+
+# Print it
+print(explanatory_data)
+
+# Import numpy with alias np
+import numpy as np
+
+# Create explanatory_data 
+explanatory_data = pd.DataFrame({'n_convenience': np.arange(0, 11)})
+
+# Use mdl_price_vs_conv to predict with explanatory_data, call it price_twd_msq
+price_twd_msq = mdl_price_vs_conv.predict(explanatory_data)
+
+# Print it
+print(price_twd_msq)
+
+# Import numpy with alias np
+import numpy as np
+
+# Create explanatory_data 
+explanatory_data = pd.DataFrame({'n_convenience': np.arange(0, 11)})
+
+# Use mdl_price_vs_conv to predict with explanatory_data, call it price_twd_msq
+price_twd_msq = mdl_price_vs_conv.predict(explanatory_data)
+
+# Create prediction_data
+prediction_data = explanatory_data.assign(price_twd_msq=mdl_price_vs_conv.predict(explanatory_data))
+
+# Print the result
+print(prediction_data)
+
+# Create a new figure, fig
+fig = plt.figure()
+
+sns.regplot(x="n_convenience",
+            y="price_twd_msq",
+            data=taiwan_real_estate,
+            ci=None)
+# Add a scatter plot layer to the regplot
+sns.scatterplot(x="n_convenience", y="price_twd_msq", data = prediction_data, color = "red")
+
+# Show the layered plot
+plt.show()
+
+# Define a DataFrame impossible
+impossible = pd.DataFrame({'n_convenience': [-1, 2.5]})
+
+mdl_price_vs_conv.predict(impossible)
+
+# Print the model parameters of mdl_price_vs_conv
+print(mdl_price_vs_conv.params)
+
+# Print the fitted values of mdl_price_vs_conv
+print(mdl_price_vs_conv.fittedvalues)
+
+# Print the residuals of mdl_price_vs_conv
+print(mdl_price_vs_conv.resid)
+
+# Print a summary of mdl_price_vs_conv
+print(mdl_price_vs_conv.summary())
+
+# Get the coefficients of mdl_price_vs_conv
+coeffs = mdl_price_vs_conv.params
+
+# Get the intercept
+intercept = coeffs[0]
+
+# Get the slope
+slope = coeffs[1]
+
+# Manually calculate the predictions
+price_twd_msq = intercept + slope * explanatory_data
+print(price_twd_msq)
+
+# Compare to the results from .predict()
+print(price_twd_msq.assign(predictions_auto=mdl_price_vs_conv.predict(explanatory_data)))
+
+# Create a new figure, fig
+fig = plt.figure()
+
+# Plot the first layer: y = x
+plt.axline(xy1=(0,0), slope=1, linewidth=2, color="green")
+
+# Add scatter plot with linear regression trend line
+sns.regplot(x = "return_2018", 
+                y = "return_2019",
+                data = sp500_yearly_returns,
+                ci = None)
+
+# Set the axes so that the distances along the x and y axes look the same
+plt.axis("equal")
+
+# Show the plot
+plt.show()
+
+# Run a linear regression on return_2019 vs. return_2018 using sp500_yearly_returns
+mdl_returns = ols("return_2019~return_2018", data = sp500_yearly_returns).fit()
+
+# Print the parameters
+mdl_returns.params
+
+mdl_returns = ols("return_2019 ~ return_2018", data=sp500_yearly_returns).fit()
+
+# Create a DataFrame with return_2018 at -1, 0, and 1 
+explanatory_data = pd.DataFrame({'return_2018': [-1, 0, 1]})
+
+# Use mdl_returns to predict with explanatory_data
+predictions = mdl_returns.predict(explanatory_data)
+print(predictions)
+
+# Create sqrt_dist_to_mrt_m
+taiwan_real_estate["sqrt_dist_to_mrt_m"] = np.sqrt(taiwan_real_estate["dist_to_mrt_m"])
+
+plt.figure()
+
+# Plot using the transformed variable
+sns.regplot(data = taiwan_real_estate, x = "sqrt_dist_to_mrt_m" , y = "price_twd_msq")
+plt.show()
+
+# Create sqrt_dist_to_mrt_m
+taiwan_real_estate["sqrt_dist_to_mrt_m"] = np.sqrt(taiwan_real_estate["dist_to_mrt_m"])
+
+# Run a linear regression of price_twd_msq vs. square root of dist_to_mrt_m using taiwan_real_estate
+mdl_price_vs_dist = ols("price_twd_msq~sqrt_dist_to_mrt_m", data = taiwan_real_estate).fit()
+
+# Print the parameters
+print(mdl_price_vs_dist.params)
+
+# Create sqrt_dist_to_mrt_m
+taiwan_real_estate["sqrt_dist_to_mrt_m"] = np.sqrt(taiwan_real_estate["dist_to_mrt_m"])
+
+# Run a linear regression of price_twd_msq vs. sqrt_dist_to_mrt_m
+mdl_price_vs_dist = ols("price_twd_msq ~ sqrt_dist_to_mrt_m", data=taiwan_real_estate).fit()
+
+explanatory_data = pd.DataFrame({"sqrt_dist_to_mrt_m": np.sqrt(np.arange(0, 81, 10) ** 2),
+                                "dist_to_mrt_m": np.arange(0, 81, 10) ** 2})
+
+# Create prediction_data by adding a column of predictions to explantory_data
+prediction_data = explanatory_data.assign(
+    price_twd_msq = mdl_price_vs_dist.predict(explanatory_data))
+
+
+# Print the result
+print(prediction_data)
+
+# Create sqrt_dist_to_mrt_m
+taiwan_real_estate["sqrt_dist_to_mrt_m"] = np.sqrt(taiwan_real_estate["dist_to_mrt_m"])
+
+# Run a linear regression of price_twd_msq vs. sqrt_dist_to_mrt_m
+mdl_price_vs_dist = ols("price_twd_msq ~ sqrt_dist_to_mrt_m", data=taiwan_real_estate).fit()
+
+# Use this explanatory data
+explanatory_data = pd.DataFrame({"sqrt_dist_to_mrt_m": np.sqrt(np.arange(0, 81, 10) ** 2),
+                                "dist_to_mrt_m": np.arange(0, 81, 10) ** 2})
+
+# Use mdl_price_vs_dist to predict explanatory_data
+prediction_data = explanatory_data.assign(
+    price_twd_msq = mdl_price_vs_dist.predict(explanatory_data)
+)
+
+fig = plt.figure()
+sns.regplot(x="sqrt_dist_to_mrt_m", y="price_twd_msq", data=taiwan_real_estate, ci=None)
+
+# Add a layer of your prediction points
+sns.scatterplot(data = prediction_data, x="sqrt_dist_to_mrt_m", y="price_twd_msq", color="red")
+plt.show()
+
+ad_conversion["qdrt_n_impressions"] = ad_conversion["n_impressions"] ** 0.25
+ad_conversion["qdrt_n_clicks"] = ad_conversion["n_clicks"] ** 0.25
+
+mdl_click_vs_impression = ols("qdrt_n_clicks ~ qdrt_n_impressions", data=ad_conversion, ci=None).fit()
+
+explanatory_data = pd.DataFrame({"qdrt_n_impressions": np.arange(0, 3e6+1, 5e5) ** .25,
+                                 "n_impressions": np.arange(0, 3e6+1, 5e5)})
+
+# Complete prediction_data
+prediction_data = explanatory_data.assign(
+    qdrt_n_clicks = mdl_click_vs_impression.predict(explanatory_data)
+)
+
+# Print the result
+print(prediction_data)
+
+# Back transform qdrt_n_clicks
+prediction_data["n_clicks"] = prediction_data["qdrt_n_clicks"] ** 4
+
+# Plot the transformed variables
+fig = plt.figure()
+sns.regplot(x="qdrt_n_impressions", y="qdrt_n_clicks", data=ad_conversion, ci=None)
+
+# Add a layer of your prediction points
+sns.scatterplot(data = prediction_data, y="qdrt_n_clicks", x="qdrt_n_impressions", color="red")
+plt.show()
+
+# Print a summary of mdl_click_vs_impression_orig
+print(mdl_click_vs_impression_orig.summary())
+
+# Print a summary of mdl_click_vs_impression_trans
+print(mdl_click_vs_impression_trans.summary())
+
+# Print the coeff of determination for mdl_click_vs_impression_orig
+print(mdl_click_vs_impression_orig.rsquared)
+
+# Print the coeff of determination for mdl_click_vs_impression_trans
+print(mdl_click_vs_impression_trans.rsquared)
+
+# Calculate mse_orig for mdl_click_vs_impression_orig
+mse_orig = mdl_click_vs_impression_orig.mse_resid
+
+# Calculate rse_orig for mdl_click_vs_impression_orig and print it
+rse_orig = np.sqrt(mdl_click_vs_impression_orig.mse_resid)
+print("RSE of original model: ", rse_orig)
+
+# Calculate mse_trans for mdl_click_vs_impression_trans
+mse_trans = mdl_click_vs_impression_trans.mse_resid
+
+# Calculate rse_trans for mdl_click_vs_impression_trans and print it
+rse_trans = np.sqrt(mdl_click_vs_impression_trans.mse_resid)
+print("RSE of transformed model: ", rse_trans)
+
+# Plot the residuals vs. fitted values
+sns.residplot(x="n_convenience",
+                y="price_twd_msq", data=taiwan_real_estate, lowess = True)
+plt.xlabel("Fitted values")
+plt.ylabel("Residuals")
+
+# Show the plot
+plt.show()
+
+# Import qqplot
+from statsmodels.api import qqplot
+
+# Create the Q-Q plot of the residuals
+qqplot(data=mdl_price_vs_conv.resid, fit=True, line="45")
+
+# Show the plot
+plt.show()
+
+# Preprocessing steps
+model_norm_residuals = mdl_price_vs_conv.get_influence().resid_studentized_internal
+model_norm_residuals_abs_sqrt = np.sqrt(np.abs(model_norm_residuals))
+
+# Create the scale-location plot
+sns.regplot(x=mdl_price_vs_conv.fittedvalues, y=model_norm_residuals_abs_sqrt, ci=None, lowess=True)
+plt.xlabel("Fitted values")
+plt.ylabel("Sqrt of abs val of stdized residuals")
+
+# Show the plot
+plt.show()
+
+# Create summary_info
+summary_info = mdl_price_vs_dist.get_influence().summary_frame()
+
+# Create summary_info
+summary_info = mdl_price_vs_dist.get_influence().summary_frame()
+
+# Add the hat_diag column to taiwan_real_estate, name it leverage
+taiwan_real_estate["leverage"] = summary_info["hat_diag"]
+
+# Sort taiwan_real_estate by leverage in descending order and print the head
+print(taiwan_real_estate.sort_values("leverage", ascending = False).head())
+
+# Create summary_info
+summary_info = mdl_price_vs_dist.get_influence().summary_frame()
+
+# Add the hat_diag column to taiwan_real_estate, name it leverage
+taiwan_real_estate["leverage"] = summary_info["hat_diag"]
+
+# Add the cooks_d column to taiwan_real_estate, name it cooks_dist
+taiwan_real_estate["cooks_dist"] = summary_info["cooks_d"]
+
+# Sort taiwan_real_estate by cooks_dist in descending order and print the head.
+print(taiwan_real_estate.sort_values("cooks_dist", ascending = False).head())
+
+# Create the histograms of time_since_last_purchase split by has_churned
+sns.displot(data = churn, x = "time_since_last_purchase", col = "has_churned")
+
+plt.show()# Redraw the plot with time_since_first_purchase
+sns.displot(data = churn, x = "time_since_first_purchase", col = "has_churned")
+
+
+plt.show()# Draw a linear regression trend line and a scatter plot of time_since_first_purchase vs. has_churned
+sns.regplot(x="time_since_first_purchase",
+            y="has_churned",
+            data=churn,
+            ci=None,
+            line_kws={"color": "red"})
+
+# Draw a logistic regression trend line and a scatter plot of time_since_first_purchase vs. has_churned
+sns.regplot(x="time_since_first_purchase",
+            y="has_churned",
+            data=churn,
+            ci=None,
+            logistic=True,
+            line_kws={"color": "blue"})
+
+plt.show()
+
+# Import logit
+from statsmodels.formula.api import logit
+
+# Fit a logistic regression of churn vs. length of relationship using the churn dataset
+mdl_churn_vs_relationship = logit("has_churned~time_since_first_purchase", data = churn).fit()
+
+# Print the parameters of the fitted model
+print(mdl_churn_vs_relationship.params)
+
+# Create prediction_data
+prediction_data = explanatory_data.assign(
+  has_churned = mdl_churn_vs_relationship.predict(explanatory_data)
+)
+
+# Print the head
+print(prediction_data.head())
+
+# Create prediction_data
+prediction_data = explanatory_data.assign(
+    has_churned = mdl_churn_vs_relationship.predict(explanatory_data)
+)
+
+fig = plt.figure()
+
+# Create a scatter plot with logistic trend line
+sns.regplot(x = "time_since_first_purchase", y = "has_churned", data = churn, ci = None, logistic=  True)
+
+# Overlay with prediction_data, colored red
+sns.scatterplot(x="time_since_first_purchase",y="has_churned",data=prediction_data,color="red")
+
+plt.show()
+
+# Update prediction data by adding most_likely_outcome
+prediction_data["most_likely_outcome"] = np.round(prediction_data["has_churned"])
+
+# Print the head
+print(prediction_data.head())
+
+# Update prediction data by adding most_likely_outcome
+prediction_data["most_likely_outcome"] = np.round(prediction_data["has_churned"])
+
+fig = plt.figure()
+
+# Create a scatter plot with logistic trend line (from previous exercise)
+sns.regplot(x="time_since_first_purchase",
+            y="has_churned",
+            data=churn,
+            ci=None,
+            logistic=True)
+
+# Overlay with prediction_data, colored red
+sns.scatterplot(x="time_since_first_purchase",y="most_likely_outcome",data=prediction_data,color="red")
+
+plt.show()
+
+# Update prediction data with odds_ratio
+prediction_data["odds_ratio"] = prediction_data["has_churned"] / (1 - prediction_data["has_churned"])
+
+# Print the head
+print(prediction_data.head())
+
+# Update prediction data with odds_ratio
+prediction_data["odds_ratio"] = prediction_data["has_churned"] / (1 - prediction_data["has_churned"])
+
+fig = plt.figure()
+
+# Create a line plot of odds_ratio vs time_since_first_purchase
+sns.lineplot(x="time_since_first_purchase",y="odds_ratio",data=prediction_data)
+
+# Add a dotted horizontal line at odds_ratio = 1
+plt.axhline(y=1, linestyle="dotted")
+
+plt.show()
+
+# Update prediction data with log_odds_ratio
+prediction_data["log_odds_ratio"] = np.log(prediction_data["odds_ratio"])
+
+# Print the head
+print(prediction_data.head())
+
+# Update prediction data with log_odds_ratio
+prediction_data["log_odds_ratio"] = np.log(prediction_data["odds_ratio"])
+
+fig = plt.figure()
+
+# Update the line plot: log_odds_ratio vs. time_since_first_purchase
+sns.lineplot(x="time_since_first_purchase",
+             y="log_odds_ratio",
+             data=prediction_data)
+
+# Add a dotted horizontal line at log_odds_ratio = 0
+plt.axhline(y=0, linestyle="dotted")
+
+plt.show()
+
+# Get the actual responses
+actual_response = churn["has_churned"]
+
+# Get the predicted responses
+predicted_response = np.round(mdl_churn_vs_relationship.predict())
+
+# Create outcomes as a DataFrame of both Series
+outcomes = pd.DataFrame({"actual_response": actual_response,"predicted_response": predicted_response})
+
+# Print the outcomes
+print(outcomes.value_counts(sort = False))
+
+# Import mosaic from statsmodels.graphics.mosaicplot
+from statsmodels.graphics.mosaicplot import mosaic
+
+# Calculate the confusion matrix conf_matrix
+conf_matrix = mdl_churn_vs_relationship.pred_table()
+
+# Print it
+print(conf_matrix)
+
+# Draw a mosaic plot of conf_matrix
+mosaic(conf_matrix)
+plt.show()
+
+# Extract TN, TP, FN and FP from conf_matrix
+TN = conf_matrix[0,0]
+TP = conf_matrix[1,1]
+FN = conf_matrix[1,0]
+FP = conf_matrix[0,1]
+
+# Calculate and print the accuracy
+accuracy = (TN + TP) / (TN + TP + FN + FP)
+print("accuracy: ", accuracy)
+
+# Calculate and print the sensitivity
+sensitivity = TP / (FN + TP)
+print("sensitivity: ", sensitivity)
+
+# Calculate and print the specificity
+specificity = TN / (TN + FP)
+print("specificity: ", specificity)
